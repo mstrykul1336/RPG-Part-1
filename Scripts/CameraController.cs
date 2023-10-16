e7d9043e359325e9019754b632687d4c2959a1e4
Mikinzi (frozencat3030) <112668967+mstrykul1336@users.noreply.github.com>
2023-10-15 18:51:00 -0700

    Add files via upload

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    void Update ()
+    {
+        if(PlayerController.me != null && !PlayerController.me.dead)
+        {
+            Vector3 targetPos = PlayerController.me.transform.position;
+            targetPos.z = -10;
+            transform.position = targetPos;
+        }
+    }
+}
